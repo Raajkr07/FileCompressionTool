aa8e022f248452767b13d9fed81517a3bf263cb0
RAJ KUMAR <rk8210032@gmail.com>
2025-09-15 20:49:33 +0530

    made spring configuration class so remove the cors annotation

--- src/com/example/compression/controller/CompressionController.java
+++ src/com/example/compression/controller/CompressionController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "*")
 public class CompressionController {
 
     private final CompressionService service;
